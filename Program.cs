ebcc4f38f811c3352d00ab6352bc547f6b6f4d65
andreyliporage <andreyliporage@gmail.com>
2021-03-07 17:58:17 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesafioSysQuestao02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            VerificaBalanceamento("{[()]}");
+            
+        }
+
+        static void VerificaBalanceamento(string s)
+        {
+            Stack<char> pilha = new Stack<char>();
+
+            char p;
+            for (int i = 0; i < s.Length; i++)
+            {
+                p = s[i];
+
+                if (p == '(')
+                    pilha.Push(p);
+                else if (p == '{')
+                    pilha.Push(p);
+                else if (p == '[')
+                    pilha.Push(p);
+                else if (p == ')')
+                    if (pilha.Count == 0)
+                    {
+                        Console.WriteLine("NAO");
+                        return;
+                    }
+                    else if (pilha.Peek() == '(')
+                        pilha.Pop();
+                    else
+                    {
+                        Console.WriteLine("NAO");
+                        return;
+                    }
+                else if (p == '}')
+                    if (pilha.Count == 0)
+                    {
+                        Console.WriteLine("NAO");
+                        return;
+                    }
+                    else if (pilha.Peek() == '{')
+                        pilha.Pop();
+                    else
+                    {
+                        Console.WriteLine("NAO");
+                        return;
+                    }
+                else if (p == ']')
+                    if (pilha.Count == 0)
+                    {
+                        Console.WriteLine("NAO");
+                        return;
+                    }
+                    else if (pilha.Peek() == '[')
+                        pilha.Pop();
+                    else
+                    {
+                        Console.WriteLine("NAO");
+                        return;
+                    }
+            }
+            Console.WriteLine(pilha.Count == 0 ? "SIM" : "NAO");
+        }
+    }
+}
